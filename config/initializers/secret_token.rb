80fd90d9cc26aa264c9cc2a949d1dae041dcae28
Jose Carmona <jcarmonaguerra@hotmail.com>
2013-04-21 16:10:25 -0500

    Commit Inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UacHelpdesk::Application.config.secret_token = 'f1dc967454931adb8ec7257391c098ad36cb665257dbfe56917c7928a8ca4171d1c3ed4446431289910e390ec5421000ee47c9e1dcd18969d48ec0a2bec747d3'
